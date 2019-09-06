4d94f58b0d7c24239ca5e5326bdf0e7a423c8b41
user <ravikiranm.35@gmail.com>
2019-09-06 02:07:30 +0100

    favic

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/favicon.ico`,
+        icon: `src/assets/favicon.gif`,
       },
     },
     `gatsby-plugin-offline`,
